63ebde3d78f3f329313e00511baf57bd4d32d826
IgorShevchik <s.hevchik.mail@gmail.com>
2024-11-02 07:55:23 +0300

    Add menu at header

--- php/quick-start/local-apps/token-storage-in-file/layouts/header.php
+++ php/quick-start/local-apps/token-storage-in-file/layouts/header.php
@@ -1,4 +1,28 @@
-<!doctype html>
+<?php
+declare(strict_types=1);
+
+$title = 'Local app with token storage';
+
+$menu = json_decode(json_encode([
+	[
+		'title' => 'API Docs',
+		'href' => 'https://apidocs.bitrix24.com'
+	],
+	[
+		'title' => '@bitrix24/b24phpsdk',
+		'href' => 'https://github.com/bitrix24/b24phpsdk'
+	],
+	[
+		'title' => '@bitrix24/b24style',
+		'href' => 'https://bitrix24.github.io/b24style/reference/colors.html'
+	],
+	[
+		'title' => '@bitrix24/b24icons',
+		'href' => 'https://bitrix24.github.io/b24icons/guide/icons.html'
+	]
+]));
+
+?><!doctype html>
 <html lang="en" dir="ltr">
 <head>
 	<meta charset="UTF-8">
@@ -10,24 +34,19 @@
 <body class="bg-tertiary font-b24-system text-base-900 antialiased">
 <div class="flex h-screen flex-col overflow-hidden">
 	<header class="relative z-10 border-b border-gray-200 bg-white">
-		<div class="container pl-0 px-4 py-4">
-			<div class="flex flex-shrink-0 items-center justify-between">
-				<span class="text-h1 mx-6">Local app with token storage</span>
-				<div class="hidden sm:flex sm:flex-row gap-2 text-gray-500">
-                    <span class="font-normal hover:text-info-text hover:underline"><a
-							href="https://apidocs.bitrix24.com/" target="_blank">API Docs</a></span>
-					<span> | </span>
-                    <span class="font-normal hover:text-info-text hover:underline"><a
-							href="https://github.com/bitrix24/b24phpsdk" target="_blank">@bitrix24/b24phpsdk</a></span>
-					<span> | </span>
-					<span class="font-normal hover:text-info-text hover:underline"><a
-							href="https://bitrix24.github.io/b24style/reference/colors.html" target="_blank">@bitrix24/b24style</a></span>
-					<span> | </span>
-					<span class="font-normal hover:text-info-text hover:underline"><a
-							href="https://bitrix24.github.io/b24icons/guide/icons.html" target="_blank">@bitrix24/b24icons</a></span>
+		<div class="px-6 py-4">
+			<div class="flex flex-shrink-0 items-center justify-between gap-6">
+				<div class="text-h2 text-nowrap truncate"><?=$title?></div>
+				<div class="hidden sm:flex sm:flex-row gap-2 text-sm text-base-500 divide-x divide-base-400 ">
+					<?php foreach($menu as $key => $menuItem):?>
+						<a
+							href="<?=$menuItem->href?>"
+							target="_blank"
+							class="whitespace-nowrap pl-2 hover:text-info-text hover:underline"
+						><?=$menuItem->title?></a>
+					<?php endforeach;?>
 				</div>
 			</div>
 		</div>
 	</header>
-    <main class="flex-auto overflow-auto">
-		
+	<main class="flex-auto overflow-auto px-6">
