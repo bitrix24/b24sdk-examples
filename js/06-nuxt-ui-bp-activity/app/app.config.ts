b753dba9c28959226fd2fdf5fea499101029c360
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-24 09:59:40 +0300

    chore(toaster): improve config

--- js/06-nuxt-ui-bp-activity/app/app.config.ts
+++ js/06-nuxt-ui-bp-activity/app/app.config.ts
@@ -1,10 +1,5 @@
 export default defineAppConfig({
 	// https://bitrix24.github.io/b24ui/guide/getting-started.html
-	toaster: {
-		position: 'top-right' as const,
-		expand: true,
-		duration: 8000
-	},
 	b24ui: {
 		colorMode: false
 	}
--- js/06-nuxt-ui-bp-activity/app/app.vue
+++ js/06-nuxt-ui-bp-activity/app/app.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+const toaster = ref({
+	position: 'top-right' as const,
+	expand: true,
+	duration: 8000
+})
 </script>
 
 <template>
-  <B24App>
+  <B24App
+    :toaster="toaster"
+  >
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
