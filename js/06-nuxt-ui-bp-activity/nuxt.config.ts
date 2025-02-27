02f9603a14249a6a492a0d69a2a59a1af2b548d5
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-27 16:49:00 +0300

    chore(config): improve

--- js/06-nuxt-ui-bp-activity/nuxt.config.ts
+++ js/06-nuxt-ui-bp-activity/nuxt.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
-import {readFileSync} from 'fs'
+import { readFileSync } from 'node:fs'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -8,39 +8,36 @@ export default defineNuxtConfig({
    * Nuxt DevTools: Failed to check parent window
    * SecurityError: Failed to read a named property '__NUXT_DEVTOOLS_DISABLE__' from 'Window'
    */
-  devtools: { enabled: false },
-  devServer: {
-    port: 3000,
-    /*/
-    host: 'custom.mydomain.local',
-    https: {
-      key: '.../source/ssl/custom.mydomain.local-key.pem',
-      cert: '.../source/ssl/custom.mydomain.local.pem'
-    },
-	//*/
-    loadingTemplate: () => {
-      return readFileSync('./template/devServer-loading.html', 'utf-8');
-    }
-  },
-  ssr: false,
 
   modules: [
     '@bitrix24/b24ui-nuxt',
     // `@bitrix24/b24jssdk-nuxt`,
     '@nuxt/eslint'
   ],
+  ssr: false, devtools: { enabled: false },
 
   css: ['~/assets/css/main.css'],
-  
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+  devServer: {
+    port: 3000,
+    // host: 'custom.mydomain.local',
+    // https: {
+    //   key: '.../source/ssl/custom.mydomain.local-key.pem',
+    //   cert: '.../source/ssl/custom.mydomain.local.pem'
+    // },
+    loadingTemplate: () => {
+      return readFileSync('./template/devServer-loading.html', 'utf-8')
+    }
   },
-  
+
   future: {
     compatibilityVersion: 4
   },
 
-  compatibilityDate: '2024-11-27'
+  compatibilityDate: '2024-11-27',
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 })
