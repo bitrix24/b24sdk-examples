614e174d1fac0a0ba2020fd5055483c735424fea
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-03-07 16:30:50 +0300

    chore(i18n): improve

--- js/06-nuxt-ui-bp-activity/nuxt.config.ts
+++ js/06-nuxt-ui-bp-activity/nuxt.config.ts
@@ -46,7 +46,6 @@ export default defineNuxtConfig({
     detectBrowserLanguage: false,
     strategy: 'no_prefix',
     lazy: true,
-    langDir: 'locales',
     defaultLocale: 'en',
     locales: [
       {
@@ -54,100 +53,100 @@ export default defineNuxtConfig({
         name: 'English',
         file: 'en.json'
       },
-      {
-        code: 'de',
-        name: 'Deutsch',
-        file: 'de.json'
-      },
-      {
-        code: 'la',
-        name: 'Español',
-        file: 'la.json'
-      },
-
-      {
-        code: 'br',
-        name: 'Português (Brasil)',
-        file: 'br.json'
-      },
-      {
-        code: 'fr',
-        name: 'Français',
-        file: 'fr.json'
-      },
-      {
-        code: 'it',
-        name: 'Italiano',
-        file: 'it.json'
-      },
-
-      {
-        code: 'pl',
-        name: 'Polski',
-        file: 'pl.json'
-      },
+      // {
+      //   code: 'de',
+      //   name: 'Deutsch',
+      //   file: 'de.json'
+      // },
+      // {
+      //   code: 'la',
+      //   name: 'Español',
+      //   file: 'la.json'
+      // },
+      //
+      // {
+      //   code: 'br',
+      //   name: 'Português (Brasil)',
+      //   file: 'br.json'
+      // },
+      // {
+      //   code: 'fr',
+      //   name: 'Français',
+      //   file: 'fr.json'
+      // },
+      // {
+      //   code: 'it',
+      //   name: 'Italiano',
+      //   file: 'it.json'
+      // },
+      //
+      // {
+      //   code: 'pl',
+      //   name: 'Polski',
+      //   file: 'pl.json'
+      // },
       {
         code: 'ru',
         name: 'Polski',
         file: 'ru.json'
       },
-      {
-        code: 'ua',
-        name: 'Українська',
-        file: 'ua.json'
-      },
-
-      {
-        code: 'tr',
-        name: 'Türkçe',
-        file: 'tr.json'
-      },
-      {
-        code: 'sc',
-        name: '中文（简体）',
-        file: 'sc.json'
-      },
-      {
-        code: 'tc',
-        name: '中文（繁體）',
-        file: 'tc.json'
-      },
-
-      {
-        code: 'ja',
-        name: '日本語',
-        file: 'ja.json'
-      },
-      {
-        code: 'vn',
-        name: 'Tiếng Việt',
-        file: 'vn.json'
-      },
-      {
-        code: 'id',
-        name: 'Bahasa Indonesia',
-        file: 'id.json'
-      },
-      {
-        code: 'ms',
-        name: 'Bahasa Melayu',
-        file: 'ms.json'
-      },
-      {
-        code: 'th',
-        name: 'ภาษาไทย',
-        file: 'th.json'
-      },
-      {
-        code: 'ar',
-        name: 'عربي',
-        file: 'ar.json'
-      },
-      {
-        code: 'kz',
-        name: 'Қазақша',
-        file: 'kz.json'
-      }
+      // {
+      //   code: 'ua',
+      //   name: 'Українська',
+      //   file: 'ua.json'
+      // },
+      //
+      // {
+      //   code: 'tr',
+      //   name: 'Türkçe',
+      //   file: 'tr.json'
+      // },
+      // {
+      //   code: 'sc',
+      //   name: '中文（简体）',
+      //   file: 'sc.json'
+      // },
+      // {
+      //   code: 'tc',
+      //   name: '中文（繁體）',
+      //   file: 'tc.json'
+      // },
+      //
+      // {
+      //   code: 'ja',
+      //   name: '日本語',
+      //   file: 'ja.json'
+      // },
+      // {
+      //   code: 'vn',
+      //   name: 'Tiếng Việt',
+      //   file: 'vn.json'
+      // },
+      // {
+      //   code: 'id',
+      //   name: 'Bahasa Indonesia',
+      //   file: 'id.json'
+      // },
+      // {
+      //   code: 'ms',
+      //   name: 'Bahasa Melayu',
+      //   file: 'ms.json'
+      // },
+      // {
+      //   code: 'th',
+      //   name: 'ภาษาไทย',
+      //   file: 'th.json'
+      // },
+      // {
+      //   code: 'ar',
+      //   name: 'عربي',
+      //   file: 'ar.json'
+      // },
+      // {
+      //   code: 'kz',
+      //   name: 'Қазақша',
+      //   file: 'kz.json'
+      // }
     ]
   }
 })
