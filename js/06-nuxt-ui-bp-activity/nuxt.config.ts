6e5ed89782daa0c2986363a02c85b885f5848b3f
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-03-07 16:19:42 +0300

    chore(i18n): init

--- js/06-nuxt-ui-bp-activity/nuxt.config.ts
+++ js/06-nuxt-ui-bp-activity/nuxt.config.ts
@@ -13,7 +13,8 @@ export default defineNuxtConfig({
     '@bitrix24/b24ui-nuxt',
     // `@bitrix24/b24jssdk-nuxt`,
     '@nuxt/eslint',
-    '@nuxt/content'
+    '@nuxt/content',
+    '@nuxtjs/i18n'
   ],
   ssr: false, devtools: { enabled: false },
 
@@ -39,6 +40,114 @@ export default defineNuxtConfig({
   vite: {
     plugins: [
       tailwindcss()
+    ]
+  },
+  i18n: {
+    detectBrowserLanguage: false,
+    strategy: 'no_prefix',
+    lazy: true,
+    langDir: 'locales',
+    defaultLocale: 'en',
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        file: 'en.json'
+      },
+      {
+        code: 'de',
+        name: 'Deutsch',
+        file: 'de.json'
+      },
+      {
+        code: 'la',
+        name: 'Español',
+        file: 'la.json'
+      },
+
+      {
+        code: 'br',
+        name: 'Português (Brasil)',
+        file: 'br.json'
+      },
+      {
+        code: 'fr',
+        name: 'Français',
+        file: 'fr.json'
+      },
+      {
+        code: 'it',
+        name: 'Italiano',
+        file: 'it.json'
+      },
+
+      {
+        code: 'pl',
+        name: 'Polski',
+        file: 'pl.json'
+      },
+      {
+        code: 'ru',
+        name: 'Polski',
+        file: 'ru.json'
+      },
+      {
+        code: 'ua',
+        name: 'Українська',
+        file: 'ua.json'
+      },
+
+      {
+        code: 'tr',
+        name: 'Türkçe',
+        file: 'tr.json'
+      },
+      {
+        code: 'sc',
+        name: '中文（简体）',
+        file: 'sc.json'
+      },
+      {
+        code: 'tc',
+        name: '中文（繁體）',
+        file: 'tc.json'
+      },
+
+      {
+        code: 'ja',
+        name: '日本語',
+        file: 'ja.json'
+      },
+      {
+        code: 'vn',
+        name: 'Tiếng Việt',
+        file: 'vn.json'
+      },
+      {
+        code: 'id',
+        name: 'Bahasa Indonesia',
+        file: 'id.json'
+      },
+      {
+        code: 'ms',
+        name: 'Bahasa Melayu',
+        file: 'ms.json'
+      },
+      {
+        code: 'th',
+        name: 'ภาษาไทย',
+        file: 'th.json'
+      },
+      {
+        code: 'ar',
+        name: 'عربي',
+        file: 'ar.json'
+      },
+      {
+        code: 'kz',
+        name: 'Қазақша',
+        file: 'kz.json'
+      }
     ]
   }
 })
