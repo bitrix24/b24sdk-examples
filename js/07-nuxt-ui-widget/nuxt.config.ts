4b7ac3c53145d261db1e35cd9e4d9f11b039182c
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-05-15 11:24:38 +0300

    chore(config): improve

--- js/07-nuxt-ui-widget/nuxt.config.ts
+++ js/07-nuxt-ui-widget/nuxt.config.ts
@@ -2,26 +2,45 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  /**
-   * @memo App work under frame
-   * Nuxt DevTools: Failed to check parent window
-   * SecurityError: Failed to read a named property '__NUXT_DEVTOOLS_DISABLE__' from 'Window'
-   */
+
+  modules: [
+    '@bitrix24/b24ui-nuxt',
+    '@nuxt/eslint',
+    '@pinia/nuxt',
+    '@bitrix24/b24jssdk-nuxt'
+  ],
   devtools: { enabled: false },
-
-  modules: ['@bitrix24/b24ui-nuxt', // `@bitrix24/b24jssdk-nuxt`,
-  '@nuxt/eslint', '@pinia/nuxt', '@bitrix24/b24jssdk-nuxt'],
-
-  css: ['~/assets/css/main.css'],
-  
-  b24ui: {
-    colorMode: false
+  devServer: {
+    port: 3000
+    // host: 'custom.mydomain.local'
+    // https: {
+    //   key: '.../source/ssl/custom.mydomain.local-key.pem',
+    //   cert: '.../source/ssl/custom.mydomain.local.pem'
+    // },
   },
+  css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    /**
+     * @memo this will be overwritten from .env
+     *
+     * @see https://nuxt.com/docs/guide/going-further/runtime-config#example
+     */
+    public: {
+      appUrl: ''
+    }
+  },
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2025-05-15',
 
   vite: {
     server: {
       // allow incoming requests from this host
-      allowedHosts: ['whale-viable-wasp.ngrok-free.app'],
+      allowedHosts: [
+        'whale-viable-wasp.ngrok-free.app'
+      ],
       // and don't forget CORS, if needed:
       cors: true
     },
@@ -29,10 +48,8 @@ export default defineNuxtConfig({
       tailwindcss()
     ]
   },
-  
-  future: {
-    compatibilityVersion: 4
-  },
 
-  compatibilityDate: '2024-11-27'
+  b24ui: {
+    colorMode: false
+  }
 })
