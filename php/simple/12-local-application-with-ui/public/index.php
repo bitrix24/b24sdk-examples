3c1ba75446275a966cfd651fbee44cf16e83dcd8
mesilov <mesilov.maxim@gmail.com>
2025-05-23 00:39:27 +0600

    Initialize local Bitrix24 app scaffolding with tooling
    Added core scaffold for a local Bitrix24 application without UI, including folder structure, dependencies (`composer.json`), environment setup (`.env`), Docker configurations, and supporting tools like linters and CI. This setup facilitates development and deployment with robust coding standards and infrastructure.
    
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

---
+++ php/simple/12-local-application-with-ui/public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the b24sdk examples package.
+ *
+ * © Maksim Mesilov <mesilov.maxim@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Controller\InstallController;
+use App\Repository\AuthRepositoryFactory;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// add to log all incoming requests
+$logger = LoggerFactory::create();
+$logger->debug('index.start', [
+    'request' => $_REQUEST,
+]);
